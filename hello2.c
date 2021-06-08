a46f529cdddfdcb4c14d92b3290903a1eb558cba
jjankovich2 <85565452+jjankovich2@users.noreply.github.com>
2021-06-08 09:25:55 -0500

    Initial commit

---
+++ hello2.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Jared Jankovich
+ * Date: 2020/06/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("JT Jankovich\n");
+  
+
+  return 0;
+}
